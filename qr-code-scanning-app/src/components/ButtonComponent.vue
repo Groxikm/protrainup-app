7bf5613c1ad814d79a101cc7bd298d51777e6f17
Groxikm <68868458+Groxikm@users.noreply.github.com>
2025-02-20 11:54:40 +0100

    init commit

---
+++ qr-code-scanning-app/src/components/ButtonComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="handleClick" class="btn">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonComponent',
+  props: {
+    label: {
+      type: String,
+      default: 'Click Me'
+    }
+  },
+  methods: {
+    handleClick() {
+      alert('Button clicked!');
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn {
+  padding: 10px 20px;
+  color: #fff;
+  background-color: #42b983;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.btn:hover {
+  background-color: #2c8f6a;
+}
+</style>
